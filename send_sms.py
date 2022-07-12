502dd7cedbe2c8b1a814381b8d505dd4b3c55d42
DeepaTeju <91202471+DeepaTeju@users.noreply.github.com>
2022-07-12 13:49:02 +0530

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 #-------------------------------------------------------
 def send(value, classes):
     #Your Account SID from twilio.com/console
-    account_sid = "AC78e6f905ba8c648fdf343e4a38f4ffc5"
+    account_sid = ""
     #Your Auth Token from twilio.com/console
-    auth_token  = "919cc7065a505dad343e8064828d17c4"
+    auth_token  = ""
 
-    client = Client("AC9c868899f0f00d91080afc0b7c4", "22543adb52ea46fb64f8a939a75da0")
+    client = Client("", "")
     message = client.messages.create(
-    to="+918861123978",
-    from_="+917899911604",
+    to="+",
+    from_="+",
     body=f"Blindness detection system report! severity level is : {value} and class is {classes}")
     #
     print('Message sent Succesfully !')
